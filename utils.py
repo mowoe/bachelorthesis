132190446df6b735a1ffd91e29cbbc1beab0b6bc
mowoe <git@mowoe.com>
2024-06-02 16:13:48 +0200

    fix extremely bad issue

--- utils.py
+++ utils.py
@@ -4,16 +4,26 @@ from huggingface_hub import hf_hub_download
 import random
 
 # Utility functions to read dataset
-
-def get_all_bounding_boxes(item):
+def get_all_bounding_boxes_and_ids(item):
     all_boxes = []
-    if "bounds" in item.keys():
-        all_boxes.append(item["bounds"])
+    if "bounds" in item.keys() and "resource-id" in item.keys():
+        all_boxes.append((item["bounds"],item["resource-id"]))
     if "children" in item.keys():
         for child in item["children"]:
-            for box in get_all_bounding_boxes(child):
+            for box in get_all_bounding_boxes_and_ids(child):
                 all_boxes.append(box)
     return all_boxes
+
+def get_all_bounding_boxes(item):
+    bboxes = get_all_bounding_boxes_and_ids(item)
+    reduced_bboxes = []
+    already_seen_ids = []
+    for box,r_id in bboxes:
+        if r_id not in already_seen_ids:
+            reduced_bboxes.append(box)
+            already_seen_ids.append(r_id)
+    return reduced_bboxes
+
 """
 returns a list of the segments and a list of coordinates
 """
@@ -25,7 +35,10 @@ def segment(image: np.ndarray, rico_json: dict)->[list,list]:
         anchor_point = (box[0],box[1])
         width = box[2]-box[0]
         height = box[3]-box[1]
-        cropped_image = image.crop((box[0],box[1],box[2],box[3]))
+        try:
+            cropped_image = image.crop((box[0],box[1],box[2],box[3]))
+        except ValueError:
+            continue
         # Only include segments that are less than 90% the size of the original image
         if (width*height) < 0.9*(image.size[0]*image.size[1]):
             segments.append(cropped_image)
@@ -42,10 +55,13 @@ def calculate_initial_theta(segment, canvas_size, original_position):
     # grid_location_x has to be in the interval [-(x_ratio-1),(x_ratio-1)], i.e. 0 means -(x_ratio - 1) and 1440 means (x_ratio - 1)
     # We can map U ~ [0, 1] to U ~ [a, b] with u -> (a - b)*u + b
     # We first map U ~ [0, max_width] to U ~ [0,1] by dividing by max_width
-    original_x_position = original_position[0] / canvas_size[0]
+
+    eps = 0.00001 # Avoid div by zero
+    
+    original_x_position = (original_position[0]) / (canvas_size[0]-segment.size[0]+eps)
     mapped_x_position = (-(x_ratio - 1) - (x_ratio - 1))*original_x_position + (x_ratio-1)
 
-    original_y_position = original_position[1] / canvas_size[1]
+    original_y_position = (original_position[1]) / (canvas_size[1]-segment.size[1]+eps)
     mapped_y_position = (-(y_ratio - 1) - (y_ratio - 1))*original_y_position + (y_ratio-1)
 
     return np.array([
@@ -53,11 +69,25 @@ def calculate_initial_theta(segment, canvas_size, original_position):
         [0.0    , y_ratio, mapped_y_position]
     ])
 
-def stack_a_on_b(a,b):
-    mask = ((1 - a[3]) * b)[:3]
-    mask = torch.cat((mask,b[3].unsqueeze(0)))
-    return torch.stack([a,mask]).sum(dim=0)
+def blend_images(image1, image2):
+    # Ensure the images are in torch tensor format and have the same shape
+    if not isinstance(image1, torch.Tensor) or not isinstance(image2, torch.Tensor):
+        raise ValueError("Both images must be torch tensors")
+    
+    if image1.shape != image2.shape:
+        raise ValueError("Both images must have the same shape")
+    
+    # Split the RGBA channels
+    r1, g1, b1, a1 = image1.unbind(dim=0)
+    r2, g2, b2, a2 = image2.unbind(dim=0)
 
+    return torch.stack([
+        r1 * a1 + r2 * (1-a1),
+        g1 * a1 + g2 * (1-a1),
+        b1 * a1 + b2 * (1-a1),
+        torch.ones_like(r1)
+    ])
+    
 def stack_alpha_aware(images):
     if len(images[0].shape) == 4:
         running_top_image = torch.zeros_like(images[0][0])
@@ -65,9 +95,14 @@ def stack_alpha_aware(images):
         running_top_image = torch.zeros_like(images[0])
     for image in images:
         if len(image.shape) == 4:
-            running_top_image = stack_a_on_b(running_top_image,image[0])
+            # running_top_image = stack_a_on_b(running_top_image,image[0])
+            # running_top_image = alpha_composite(running_top_image, image[0])
+            running_top_image = blend_images(image[0],running_top_image)
         else:
-            running_top_image = stack_a_on_b(running_top_image,image)
+            # running_top_image = stack_a_on_b(running_top_image,image)
+            # running_top_image = alpha_composite(running_top_image, image)
+            running_top_image = blend_images(image,running_top_image)
+        # display(resize_image(transform_t_to_pil(running_top_image)))
     return running_top_image
 
 class AestheticPredictor(torch.nn.Module):
@@ -96,7 +131,7 @@ class AestheticPredictor(torch.nn.Module):
 
 
 
-def get_random_initial_position(segment, canvas_size, original_position, seed=1):
+def get_random_initial_position(segment, canvas_size, original_position, seed=1, verbose=0):
     # random.seed(seed)
 
     
@@ -107,11 +142,12 @@ def get_random_initial_position(segment, canvas_size, original_position, seed=1)
     mapped_x_position = random.uniform(-(x_ratio-1), (x_ratio-1))
     mapped_y_position = random.uniform(-(y_ratio-1), (y_ratio-1))
 
-    print(f"Original would have been: {calculate_initial_theta(segment,canvas_size,original_position)}")
-    print(f"""Now is {np.array([ 
-        [x_ratio, 0.0    , mapped_x_position],
-        [0.0    , y_ratio, mapped_y_position]
-    ])}""")
+    if verbose:
+        print(f"Original would have been: {calculate_initial_theta(segment,canvas_size,original_position)}")
+        print(f"""Now is {np.array([ 
+            [x_ratio, 0.0    , mapped_x_position],
+            [0.0    , y_ratio, mapped_y_position]
+        ])}""")
 
     
     return np.array([
